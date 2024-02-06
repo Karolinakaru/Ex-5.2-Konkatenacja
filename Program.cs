fa884a3496160ce09eb123952cf705efbd16c30b
singhet <singhet@SCSWL4072>
2024-02-06 09:32:44 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        string firstString, secondString, linkedString;
+        Console.WriteLine("Enter the first string and press ENTER");
+        firstString = Console.ReadLine();
+        Console.WriteLine("Enter the second string press ENTER");
+        secondString = Console.ReadLine();
+        Console.WriteLine("Concatenated strings are:");
+        linkedString = string.Concat(firstString, " ", secondString);//concatenate two text strings
+        Console.WriteLine(linkedString);
+        Console.ReadKey();
+    }
+}
